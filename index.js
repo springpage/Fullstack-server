39969f69127aab8cd5ed639fd5f97f13bbf1f667
Spring Page <spring616666@gmail.com>
2018-06-25 08:59:52 -0700

    12

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+
+const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
---
+++ sendgrid_webhook.js
@@ -0,0 +1,4 @@
+var localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: "sdf5sdf5ds26sd2gsd5" }, function(err, tunnel) {
+  console.log('LT running')
+});
